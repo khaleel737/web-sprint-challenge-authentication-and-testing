5e49d6977a1ea176474803c480b19e5c8e513e27
Khaleel Musleh <khaleelmusleh@Khaleels-MacBook-Pro.local>
2022-02-25 20:00:37 -0800

    test

--- api/server.test.js
+++ api/server.test.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 const db = require('../data/dbConfig');
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
 
 beforeAll(async () => {
